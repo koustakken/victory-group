dfe4e18f1190fe808946e54fe4590d2f5fd584d9
koustakken <kous.takken@gmail.com>
2024-09-26 17:45:36 +0500

    feat: complete desktop

--- src/js/slide.js
+++ src/js/slide.js
@@ -10,3 +10,62 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 })
+
+const secondSwiper = new Swiper('.swiper-2', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const thirdSwiper = new Swiper('.swiper-3', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const fourthSwiper = new Swiper('.swiper-4', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const fifthSwiper = new Swiper('.swiper-5', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const sixthSwiper = new Swiper('.swiper-6', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const seventhSwiper = new Swiper('.swiper-7', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+const eighthSwiper = new Swiper('.swiper-8', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  allowTouchMove: false,
+})
+
+swiper.controller.control = [
+  secondSwiper,
+  thirdSwiper,
+  fourthSwiper,
+  fifthSwiper,
+  sixthSwiper,
+  seventhSwiper,
+  eighthSwiper,
+]
