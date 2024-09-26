8e34860f016c44fa3633fe3357966a9416eecec6
koustakken <kous.takken@gmail.com>
2024-09-26 15:11:53 +0500

    feat: first slider

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,3 +1,12 @@
-import $ from 'jquery'
+import Swiper from 'swiper/bundle'
+import 'swiper/css/bundle'
 
-$(function () {})
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  loop: true,
+  spaceBetween: 12,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
